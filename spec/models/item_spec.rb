7278b2445234239b2a142b643aaf3780d95b088d
Annie Baer <albaer@gmail.com>
2015-07-31 00:41:33 -0400

    Added model tests to make sure factory builds are valid

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Item, :type => :model do
+  it { expect(FactoryGirl.build(:item)).to be_valid }
+  it { expect(FactoryGirl.create(:item)).to be_valid }
+
   it { should belong_to :list }
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe List, :type => :model do
+  it { expect(FactoryGirl.build(:list)).to be_valid }
+  it { expect(FactoryGirl.create(:list)).to be_valid }
+
   it { should have_many :items }
 end
