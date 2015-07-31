56c04774490fe49d0618370673e53a6fb33ad241
Annie Baer <albaer@gmail.com>
2015-07-30 23:38:34 -0400

    Added breakfast food seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "----------------------------"
+puts "Purging lists and items..."
+List.destroy_all
+Item.destroy_all
+puts "----------------------------"
+puts "Seeding Breakfast..."
+
+breakfast = List.create!(title: "Breakfast Foods")
+breakfast_item_attributes = [
+  {text: "French Toast", position: 1},
+  {text: "Pancakes", position: 2},
+  {text: "Omelet", position: 3},
+  {text: "Hash Browns", position: 4},
+  {text: "Scrambled Eggs", position: 5},
+  {text: "Bacon", position: 6},
+  {text: "Waffles", position: 7}
+]
+breakfast_item_attributes.each do |attrs|
+  item = Item.create!(attrs)
+  breakfast.items << item
+end
+puts "----------------------------"
+puts "Done!"
+puts "----------------------------"
